3760711ed334f12dfc8293da81c834fe1754366e
MilenaLiatetskayaBSU <149940486+MilenaLiatetskayaBSU@users.noreply.github.com>
2023-11-05 15:52:24 +0300

    Add files via upload

---
+++ 2nd_year/Operating_Systems/lab2/ConsoleApplication1.cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <conio.h>
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+int main()
+{
+	char lpszComLine[500] = "ConsoleApplication2.exe ";
+	char lpszHandle[20];
+	wchar_t wComLine[180];
+
+	char str[500];
+	char str3[] = " ";
+
+	cout << "Enter array size ";
+	cin.getline(str, 500);
+	int s = stoi(str) + 2;
+	string size = to_string(s);
+	strcpy(str, "");
+	strcat(str, size.c_str());
+	strcat(lpszComLine, str);
+	cout << endl<<"Enter array "<<endl;
+	strcpy(str, "");
+	cin.getline(str, 500);
+	strcat(lpszComLine, " ");
+	strcat(lpszComLine, str);
+	strcpy(str, "");
+	cout << endl << "enter the search range (2 numbers) " << endl;
+	cin.getline(str, 500);
+	strcat(lpszComLine, " ");
+	strcat(lpszComLine, str);
+	
+    //cout << lpszComLine;
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+
+	si.dwX = 400; // this member is the x offset of the upper left corner of a window if a new window is created
+
+	strcat(lpszComLine, lpszHandle);
+	MultiByteToWideChar(CP_ACP, 0, lpszComLine, strlen(lpszComLine), wComLine, 160);
+
+	CreateProcess( NULL, wComLine, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	return 0;
+}
---
+++ 2nd_year/Operating_Systems/lab2/ConsoleApplication2.cpp
@@ -0,0 +1,55 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <conio.h>
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+int main(int argc, char* argv[])
+{
+	int size = stoi(argv[1]);
+	int* mas = new int[size];
+	int k = 2;
+	int i = 0;
+	for (i; i < size; i++)
+	{
+		mas[i] = stoi(argv[k]);
+		k++;
+		cout << mas[i] << " ";
+	}
+
+	int A = mas[size - 2];
+	int B = mas[size - 1];
+
+	size = size - 2;
+
+	int* mas2 = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		mas2[i] = 0;
+	}
+
+	k = 0;
+
+	cout << endl<< A << " " << B << endl;
+
+	for (int i = 0; i < size; i++)
+	{
+		if ((mas[i] <= B) && (mas[i] >= A))
+		{
+			mas2[k] = mas[i];
+			k++;
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << mas2[i]<<" ";
+	}
+
+
+	_getch();
+}
+
+
