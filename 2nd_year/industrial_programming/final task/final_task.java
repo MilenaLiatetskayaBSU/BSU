17072b3de9b98a182537df3b1e37b2d5747e042f
MilenaLiatetskayaBSU <149940486+MilenaLiatetskayaBSU@users.noreply.github.com>
2024-01-01 17:50:53 +0300

    Add files via upload

--- 2nd_year/industrial_programming/final task/final_task.java
+++ 2nd_year/industrial_programming/final task/final_task.java
@@ -1,5 +1,11 @@
 package final_task;
 
-public class final_task {
+import java.io.IOException;
 
+public class final_task {
+	public static void main(String[] args) throws IOException
+	{
+		Parse_by_regular a = new Parse_by_regular();
+		System.out.println(a.calculate("2*2+5"));
+	}
 }
