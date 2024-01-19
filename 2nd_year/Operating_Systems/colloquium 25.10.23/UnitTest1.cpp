677be5a109d39fd6191d07bf4dcc17456625a520
Milena <149940486+MilenaLiatetskayaBSU@users.noreply.github.com>
2024-01-19 19:17:27 +0300

    Add files via upload

---
+++ 2nd_year/Operating_Systems/colloquium 25.10.23/UnitTest1.cpp
@@ -0,0 +1,43 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab1/lab1.cpp"
+#include <vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+		
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+			vector<int>result(4);
+			result = GetResult(3);
+
+			string expected = "1 1 2 6 ";
+			string res = "";
+
+			for (int i : result)
+			{
+				res += std::to_string(i) + " ";
+			}
+			Assert::AreEqual(expected, res);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			string expected = "Invalid size";
+
+			std::stringstream input("-5");
+			std::stringstream output;
+
+			cin.rdbuf(input.rdbuf());
+			cout.rdbuf(output.rdbuf());
+			int a = main();
+
+			Assert::AreEqual(expected, output.str().substr(16));
+		}
+	};
+}
